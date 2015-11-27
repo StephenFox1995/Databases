9604760e9f82bb6f9fc7fe9fb16a9040f58da977
Stephen Fox <stephenfox995@hotmail.com>
2015-11-27 17:46:02 +0000

    Finished triggers

--- game_admin_id_trigger.sql
+++ game_admin_id_trigger.sql
@@ -10,3 +10,34 @@ BEGIN
       (-20000, ' GAMEADMIN.GAMEADMINID: Admin does not have correct prefix. Must begin with A');
   END IF;  
 END;
+
+
+--CREATE OR REPLACE TRIGGER gameadmin_ai
+--AFTER INSERT ON gameadmin
+--FOR EACH ROW 
+--BEGIN
+--  INSERT INTO CHALLENGE_LOGTABLE values('gameadmin table', 
+--  :new.gameadminid, 
+--  TO_CHAR(user),
+--  'INS',
+--  sysdate);
+--END;
+
+DROP TABLE CHALLENGE_LOGTABLE;
+
+CREATE TABLE CHALLENGE_LOGTABLE (
+  tablename varchar(20),
+  gameadminid varchar2(4),
+  userLogin varchar(255),
+  auditor char(3) CHECK (auditor IN ('INS','UPD','DEL')),
+  sys_date DATE
+);
+
+delete from challenge where challengeid = 'C006';
+delete from gameadmin where gameadminid = 'A010';
+insert into challenge values('C006', 'Football', '09:09:12');
+insert into gameadmin values('A010', 'C006');
+
+drop table challenge_logtable;
+
+select * from challenge_logtable;
--- selectiosn.sql
+++ selectiosn.sql
@@ -8,16 +8,21 @@ SELECT player.playerid, tool.toolname FROM player
 INNER JOIN tool ON player.playerid = tool.playerid
 WHERE player.playerid = 'P001';
 
-SELECT * FROM leaderboard;
+-- Outer join
+-- All information on player and all tools they've used.
+SELECT player.*, tool.* FROM player
+FULL OUTER JOIN tool ON player.playerid = tool.playerid
+WHERE player.playerid = 'P001';
 
--- Aggregate & sub query.
+
+-- Aggregate & sub query
+-- Finds all the info about the player who has the highscore.
 SELECT player.name, player.playerid, player.points, leaderboard.score from player 
 JOIN leaderboard ON player.playerid = leaderboard.playerid
-WHERE playerid in 
-  (SELECT playerid FROM leaderboard 
+WHERE leaderboard.playerid in 
+  (SELECT leaderboard.playerid FROM leaderboard 
    WHERE score in (
-    SELECT MAX(score) FROM Leaderboard)
-  );
+    SELECT MAX(score) FROM Leaderboard));
 
 
 -- Union
